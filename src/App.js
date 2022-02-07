3a14ab383f32bb830bcf3e7300f19c074ece859e
jordanshear <jordan.shear@koalafi.com>
2022-02-07 09:28:39 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import { TaskList } from './components/TaskList';
+import TasklistStories from './components/Tasklist.stories';
+
+import './index.css';
+import store from './lib/store';
+
+import { Provider } from 'react-redux';
+import { InboxScreen } from './components/InboxScreen';
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //     <TaskList/>
+    //   </header>
+    // </div>
+    <Provider store={store}>
+      <InboxScreen />
+    </Provider>
+  );
+}
+
+export default App;
